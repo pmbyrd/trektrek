1ac00d1f7daaa924f49c5966e0c193f03c5cf367
Perriesha <pmbyrd@gmail.com>
2023-08-13 20:12:20 -0700

    Changed .env vars again

--- config.py
+++ config.py
@@ -1,28 +1,13 @@
 import os
 from dotenv import load_dotenv
-basedir = os.path.abspath(os.path.dirname(__file__))
+BASEDIR = os.path.abspath(os.path.dirname(__file__))
+db_username = os.getenv('POSTGRES_USER')
+db_password = os.getenv('POSTGRES_PASSWORD')
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-
-class DevelopmentConfig(Config):
-    DEBUG = True
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URI', 'postgresql:///trektrek')
-
-class ProductionConfig(Config):
-    DEBUG = False
-    SQLALCHEMY_DATABASE_URI = os.environ.get('PROD_DATABASE_URI', 'postgresql:///trektrek_prod').replace("postgres://", "postgresql://", 1)
-
-config_by_name = {
-    'development': DevelopmentConfig,
-    'production': ProductionConfig,
-    # Add more configurations if needed
-}
-
-load_dotenv()
-
-# Select the appropriate configuration based on the FLASK_ENV environment variable
-config = config_by_name.get(os.getenv('FLASK_ENV', 'development'))
-
-
+    if os.getenv('DATABASE_URL'):
+        SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL').replace("postgres://", "postgresql://", 1)
+    elif os.getenv('DATABASE_URL') is None:
+        SQLALCHEMY_DATABASE_URI = f"postgresql://{db_username}:{db_password}@localhost/trektrek"    
+    else:
+        f"sqlite:///{os.path.join(BASEDIR, 'instance', 'app.db')}"
