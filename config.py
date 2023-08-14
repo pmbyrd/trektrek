275ad7bf94f6b6f0f70d575232ae57d6adad78cf
Perriesha <pmbyrd@gmail.com>
2023-08-13 21:54:08 -0700

    Tried something new for whatever it is worth

--- config.py
+++ config.py
@@ -4,10 +4,39 @@ BASEDIR = os.path.abspath(os.path.dirname(__file__))
 db_username = os.getenv('POSTGRES_USER')
 db_password = os.getenv('POSTGRES_PASSWORD')
 
-class Config:
-    if os.getenv('DATABASE_URL'):
+# class Config:
+#     if os.getenv('DATABASE_URL'):
+#         SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL').replace("postgres://", "postgresql://", 1)
+#     elif os.getenv('DATABASE_URL') is None:
+#     else:
+#         f"sqlite:///{os.path.join(BASEDIR, 'instance', 'app.db')}"
+        
+#     LOG_WITH_GUNICORN = os.getenv('LOG_WITH_GUNICORN', default=False)
+
+class Config(object):
+    FLASK_ENV = 'development'
+    DEBUG = True
+    TESTING = False
+    SECRET_KEY = os.getenv('SECRET_KEY', default='you-will-never-guess')
+    if os.getenv('DEV_DATABASE_URL'):
         SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL').replace("postgres://", "postgresql://", 1)
-    elif os.getenv('DATABASE_URL') is None:
-        SQLALCHEMY_DATABASE_URI = f"postgresql://{db_username}:{db_password}@localhost/trektrek"    
     else:
-        f"sqlite:///{os.path.join(BASEDIR, 'instance', 'app.db')}"
+        SQLALCHEMY_DATABASE_URI = f"postgresql://{db_username}:{db_password}@localhost/trektrek"   
+        
+    LOG_WITH_GUNICORN = os.getenv('LOG_WITH_GUNICORN', default=False)
+
+ 
+
+class ProductionConfig(Config):
+    FLASK_ENV = 'production'
+
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+
+
+class TestingConfig(Config):
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI = os.getenv('TEST_DATABASE_URI',
+                                        default=f"sqlite:///{os.path.join(BASEDIR, 'instance', 'test.db')}")
+    WTF_CSRF_ENABLED = False
